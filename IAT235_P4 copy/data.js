7ca2ded3cdfc28cf1877cf639aa04d65f93d778a
sopialeee <96020242+sopialeee@users.noreply.github.com>
2021-12-12 10:41:57 -0800

    HTML(pages)

---
+++ IAT235_P4 copy/data.js
@@ -0,0 +1,115 @@
+// List of objects containing titles & descriptions associated with each image
+const data = [
+    {
+        title: "Barriers",
+        description: "These low-gravity concrete barriers regulate traffic flow and discourage U-turn activity."
+    },
+    {
+        title: "Bicycle racks",
+        description: "Installation of bike racks provide secure bike parking at schools."
+    },
+    {
+        title: "Bus pad",
+        description: "Improves access for people using wheelchairs, walkers, and strollers to get to the bus waiting area."
+    },
+   
+    {
+        title: "Curb bulge",
+        description: "Curb budges pull together the two sides of the streets together for pedestrians. They shorten the distance a pedestrian is actually on the asphalt, and increase visibility for pedestrians waiting to cross by providing them a perch or a pedestal to be prominently seen by oncoming drivers."
+    },
+
+    {
+        title: "Curb ramps",
+        description:"Curb ramps and bus pads improve accessibility for people using wheelchairs, walkers, and strollers. "
+    },
+
+    {
+        title: "HUB’s Bike to School week",
+        description: "The Launch of HUB's Bike to School week included many activities to encourage young children to partake in cycling as a form of active travel.  HUB and City of Vancouver officials were present and children were encouraged to participate in bike tune-ups and decorating sessions."
+    },
+   
+    {
+        title: "New marked and raised crosswalks",
+        description: "Crosswalks alert people driving to expect people walking. Raised crosswalks reduce vehicle speed and increases pedestrian visibility."
+    },
+
+    {
+        title: "New sidewalks and curbs",
+        description: "Sidewalk segments address gaps in the walking network. Sidewalks and curbs provide a continuous walking experience separating people from traffic. "
+    },
+
+    {
+        title: "Protected bicycle lane",
+        description: "On-street bike facility physically separated from motor vehicles. Can be elevated or protected by various treatments such as a curb, median, and/or bollards. "
+    },
+
+    {
+        title: "Pedestrian/bike activated signals",
+        description: "Assists pedestrians and cyclists in crossing major streets by providing signal-protected pedestrian crossing phase"
+    },
+
+    {
+        title: "Pedestrian decal - Safety tips",
+        description: "Installed above the pedestrian crossing button, this placard reminds users of the correct crossing etiquette.  Remember, pedestrians should only ever enter an intersection when the white crossing silhouette is illuminated."
+    },
+
+    {
+        title: "Pedestrian signal crossing improvement",
+        description: "Countdown timers show how much time is left to cross the street at an intersection. This reduces the number of people crossing the intersection when the light changes."
+    },
+
+    {
+        title: "Rapid flashing beacons",
+        description: "These high intensity amber flashing beacons installed at crosswalks, activated by a pedestrian push-button. The beacons increase pedestrian visibility and alert people driving to yield at marked crosswalks. "
+    },
+
+    {
+        title: "Signage",
+        description: "Clarifies pick-up/drop-off operations, indicates various parking regulations, and improves sight-lines."
+    },
+
+    {
+        title: "Speed humps",
+        description: "Speed humps are a traffic calming measure used to reduce vehicle volume and speed on local streets. "
+
+    },
+
+    {
+        title: "Street mural",
+        description: "Street murals are a school or community-led initiative, supported by the City, to apply artwork to the street as a traffic-calming measure. "
+    },
+
+    {
+        title: "Student art",
+        description: "Signs and artworks created by students raise awareness of travel safety around the school. "
+    },
+
+    {
+        title: "Will’s Jams Concert",
+        description: "Young children at Franklin Elementary were rocking out at the Will's Jams concert, part of the Grandma on the Move school tour."
+
+    }
+
+]
+
+// List of all image files
+const images = [
+    "img/barriers.jpeg",
+    "img/bicycle_racks.jpeg",
+    "img/Bus_pad.jpeg",
+    "img/curb_bulge.jpeg",
+    "img/curd_ramps.jpeg",
+    "img/Hub's_bike_to_school_week.jpeg",
+    "img/new_marked_and_raised_crosswalks.jpeg",
+    "img/new_sidewalks_and_curbs.jpeg",
+    "img/protected_bicycle_lane.jpeg",
+    "img/pedestrian_bike_activated_signals.jpeg",
+    "img/pedistrian_decal.jpeg",
+    "img/pedestrian_signalized_crossing_improvement.jpeg",
+    "img/rapid_flashing_beacons.jpeg",
+    "img/signage.jpeg",
+    "img/speed_humps.jpeg",
+    "img/street_mural.jpeg",
+    "img/student_art.jpeg",
+    "img/will's_jams_concert.jpeg",
+]
